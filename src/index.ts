f8d2b3b6e4f02d7a278ca40f8fcbda5040e4a805
Tamara Jordan <tamara.jordan1+coding@hotmail.com>
2020-01-01 14:52:20 +0000

    Extract test logic to test command module

--- src/index.ts
+++ src/index.ts
@@ -11,6 +11,7 @@ import SchemaLoader from './validation/schema-loader'
 import createServeCommand from './serve/command'
 import { HandleError, CreateMain } from './command-shared'
 import createGenerateCommand from './generate/command'
+import createTestCommand from './test/command'
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 const handleError: HandleError = ({ stack, message }) => {
@@ -35,55 +36,9 @@ export default async function run(): Promise<void> {
   // TODO: figure out how I can remove this
   // eslint-disable-next-line no-unused-expressions
   mainYargs
-    .command(createServeCommand(handleError, createMain))
-    .command(
-      'test <configPath> <baseURL>',
-      'Tests API endpoint responses against a json schema',
-      yargs =>
-        yargs
-          .option('allErrors', {
-            alias: 'a',
-            type: 'boolean',
-            description: 'show all validation errors per test instead of failing fast',
-            default: false,
-          })
-          .option('schemaPath', {
-            type: 'string',
-            description: 'specify a path to load json schemas from, rather than generating them',
-          })
-          .option('tsconfigPath', {
-            alias: 'c',
-            type: 'string',
-            description: 'a path to the tsconfig which contains required symbols',
-            default: './tsconfig.json',
-          })
-          .positional('configPath', {
-            describe: 'path to the mock config',
-            type: 'string',
-          })
-          .positional('baseURL', {
-            describe: 'the URL that your endpoints should be accessed through',
-            type: 'string',
-          }),
-      ({ configPath, baseURL, allErrors, tsconfigPath, schemaPath }) => {
-        if (!configPath || !baseURL) process.exit(1)
-
-        let testConfigs: TestConfig[]
-        try {
-          testConfigs = readConfig(configPath).filter(x => x.request.endpoint)
-        } catch (err) {
-          return handleError(err)
-        }
-
-        if (!testConfigs.length) return console.log('No tests to run')
-
-        createMain(allErrors, tsconfigPath, schemaPath)
-          .test(baseURL, createClient(axios.create({ baseURL })), testConfigs)
-          .then(() => process.exit())
-          .catch(handleError)
-      },
-    )
     .command(createGenerateCommand(handleError))
+    .command(createServeCommand(handleError, createMain))
+    .command(createTestCommand(handleError, createMain))
     .example(
       'ncdc serve ./service-config.json 4000',
       'Serves the mock API endpoints defined in service-config.json on port 4000',
---
+++ src/test/command.ts
@@ -0,0 +1,71 @@
+import { HandleError, CreateMain } from '../command-shared'
+import { Argv, CommandModule } from 'yargs'
+import readConfig, { TestConfig } from '../config'
+import { createClient } from './http-client'
+import axios from 'axios'
+
+interface TestArgs {
+  allErrors: boolean
+  schemaPath?: string
+  tsconfigPath: string
+  configPath?: string
+  baseURL?: string
+}
+
+const builder = (yargs: Argv): Argv<TestArgs> =>
+  yargs
+    .option('allErrors', {
+      alias: 'a',
+      type: 'boolean',
+      description: 'show all validation errors per test instead of failing fast',
+      default: false,
+    })
+    .option('schemaPath', {
+      type: 'string',
+      description: 'specify a path to load json schemas from, rather than generating them',
+    })
+    .option('tsconfigPath', {
+      alias: 'c',
+      type: 'string',
+      description: 'a path to the tsconfig which contains required symbols',
+      default: './tsconfig.json',
+    })
+    .positional('configPath', {
+      describe: 'path to the mock config',
+      type: 'string',
+    })
+    .positional('baseURL', {
+      describe: 'the URL that your endpoints should be accessed through',
+      type: 'string',
+    })
+
+const createHandler = (handleError: HandleError, createMain: CreateMain) => (args: TestArgs): void => {
+  const { configPath, baseURL, allErrors, tsconfigPath, schemaPath } = args
+  if (!configPath || !baseURL) process.exit(1)
+
+  let testConfigs: TestConfig[]
+  try {
+    testConfigs = readConfig(configPath).filter(x => x.request.endpoint)
+  } catch (err) {
+    return handleError(err)
+  }
+
+  if (!testConfigs.length) return console.log('No tests to run')
+
+  createMain(allErrors, tsconfigPath, schemaPath)
+    .test(baseURL, createClient(axios.create({ baseURL })), testConfigs)
+    .then(() => process.exit())
+    .catch(handleError)
+}
+
+export default function createTestCommand(
+  handleError: HandleError,
+  createMain: CreateMain,
+): CommandModule<{}, TestArgs> {
+  return {
+    command: 'test <configPath> <baseURL>',
+    describe: 'Tests API endpoint responses against a json schema',
+    builder,
+    handler: createHandler(handleError, createMain),
+  }
+}

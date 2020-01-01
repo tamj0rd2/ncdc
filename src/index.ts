d325fed509617e20fac51385524a66a609bb54bf
Tamara Jordan <tamara.jordan1+coding@hotmail.com>
2020-01-01 14:37:48 +0000

    Refactor serve to use command module

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,24 @@
 import mainYargs from 'yargs'
-import yargs from 'yargs'
 import Main from './main'
 import TypeValidator from './validation/type-validator'
 import ajv from 'ajv'
 import SchemaGenerator from './validation/schema-generator'
 import chalk from 'chalk'
-import readConfig, { MockConfig, TestConfig } from './config'
-import { resolve, normalize } from 'path'
+import readConfig, { TestConfig } from './config'
 import { createClient } from './test/http-client'
 import axios from 'axios'
-import IOClient from './serve/io-client'
 import { generate } from './generate/generate'
 import SchemaLoader from './validation/schema-loader'
+import createServeCommand from './serve/command'
+import { HandleError, CreateMain } from './command-shared'
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-const handleError = ({ stack, message }: Error): never => {
+const handleError: HandleError = ({ stack, message }) => {
   console.error(chalk.red(message))
   process.exit(1)
 }
 
-const createMain = (allErrors: boolean, tsconfigPath: string, schemaPath: Optional<string>): Main => {
+const createMain: CreateMain = (allErrors, tsconfigPath, schemaPath) => {
   try {
     return new Main(
       new TypeValidator(
@@ -33,81 +32,10 @@ const createMain = (allErrors: boolean, tsconfigPath: string, schemaPath: Option
 }
 
 export default async function run(): Promise<void> {
+  // TODO: figure out how I can remove this
   // eslint-disable-next-line no-unused-expressions
   mainYargs
-    // TODO: look into providing command modules. They should help tidy things up
-    // https://github.com/yargs/yargs/blob/master/docs/advanced.md
-    .command(
-      'serve <configPath> [port]',
-      'Serves mock API responses',
-      yargs =>
-        yargs
-          .option('allErrors', {
-            alias: 'a',
-            type: 'boolean',
-            description: 'show all validation errors per test instead of failing fast',
-            default: false,
-          })
-          .option('schemaPath', {
-            type: 'string',
-            description: 'specify a path to load json schemas from, rather than generating them',
-          })
-          .option('tsconfigPath', {
-            alias: 'c',
-            type: 'string',
-            description: 'a path to the tsconfig which contains required symbols',
-            default: './tsconfig.json',
-          })
-          .positional('configPath', {
-            describe: 'path to the mock config',
-            type: 'string',
-          })
-          .positional('port', {
-            describe: 'port to serve the API on',
-            type: 'number',
-            default: 4000,
-          }),
-      ({ configPath, port, allErrors, tsconfigPath, schemaPath }) => {
-        if (!configPath) process.exit(1)
-
-        if (isNaN(port)) {
-          yargs.showHelp()
-          console.error('\nport must be a number')
-          return process.exit(1)
-        }
-
-        const fullConfigPath = resolve(configPath)
-        const configFolder = normalize(`${fullConfigPath}/../`)
-
-        let mockConfigs: MockConfig[]
-        try {
-          mockConfigs = readConfig<MockConfig>(fullConfigPath)
-            .filter(x => x.response.mockPath || x.response.mockBody || x.response.body)
-            .map(config => {
-              // TODO: this feels a bit naughty
-              const mockPath = config.response.mockPath
-              if (!mockPath) return config
-
-              return {
-                ...config,
-                response: {
-                  ...config.response,
-                  mockPath: /^.?.\//.test(mockPath) ? resolve(configFolder, mockPath) : mockPath,
-                },
-              }
-            })
-        } catch (err) {
-          return handleError(err)
-        }
-
-        if (!mockConfigs.length) return console.log('No mocks to run')
-
-        createMain(allErrors, tsconfigPath, schemaPath)
-          .serve(port, mockConfigs, new IOClient())
-          .then(() => process.exit())
-          .catch(handleError)
-      },
-    )
+    .command(createServeCommand(handleError, createMain))
     .command(
       'test <configPath> <baseURL>',
       'Tests API endpoint responses against a json schema',
---
+++ src/serve/command.ts
@@ -0,0 +1,96 @@
+import { Argv } from 'yargs'
+import yargs = require('yargs')
+import { resolve, normalize } from 'path'
+import readConfig, { MockConfig } from '../config'
+import IOClient from './io-client'
+import { HandleError, CreateMain } from '../command-shared'
+
+interface ServeArgs {
+  configPath?: string
+  port: number
+  allErrors: boolean
+  tsconfigPath: string
+  schemaPath?: string
+}
+
+const builder = (yargs: Argv): Argv<ServeArgs> =>
+  yargs
+    .option('allErrors', {
+      alias: 'a',
+      type: 'boolean',
+      description: 'show all validation errors per test instead of failing fast',
+      default: false,
+    })
+    .option('schemaPath', {
+      type: 'string',
+      description: 'specify a path to load json schemas from, rather than generating them',
+    })
+    .option('tsconfigPath', {
+      alias: 'c',
+      type: 'string',
+      description: 'a path to the tsconfig which contains required symbols',
+      default: './tsconfig.json',
+    })
+    .positional('configPath', {
+      describe: 'path to the mock config',
+      type: 'string',
+    })
+    .positional('port', {
+      describe: 'port to serve the API on',
+      type: 'number',
+      default: 4000,
+    })
+
+const createHandler = (handleError: HandleError, createMain: CreateMain) => (args: ServeArgs): void => {
+  const { configPath, port, allErrors, tsconfigPath, schemaPath } = args
+  if (!configPath) process.exit(1)
+
+  if (isNaN(port)) {
+    yargs.showHelp()
+    console.error('\nport must be a number')
+    return process.exit(1)
+  }
+
+  const fullConfigPath = resolve(configPath)
+  const configFolder = normalize(`${fullConfigPath}/../`)
+
+  let mockConfigs: MockConfig[]
+  try {
+    mockConfigs = readConfig<MockConfig>(fullConfigPath)
+      .filter(x => x.response.mockPath || x.response.mockBody || x.response.body)
+      .map(config => {
+        // TODO: this feels a bit naughty
+        const mockPath = config.response.mockPath
+        if (!mockPath) return config
+
+        return {
+          ...config,
+          response: {
+            ...config.response,
+            mockPath: /^.?.\//.test(mockPath) ? resolve(configFolder, mockPath) : mockPath,
+          },
+        }
+      })
+  } catch (err) {
+    return handleError(err)
+  }
+
+  if (!mockConfigs.length) return console.log('No mocks to run')
+
+  createMain(allErrors, tsconfigPath, schemaPath)
+    .serve(port, mockConfigs, new IOClient())
+    .then(() => process.exit())
+    .catch(handleError)
+}
+
+export default function createServeModule(
+  handleError: HandleError,
+  createMain: CreateMain,
+): yargs.CommandModule<{}, ServeArgs> {
+  return {
+    command: 'serve <configPath> [port]',
+    describe: 'Serves mock API responses',
+    builder,
+    handler: createHandler(handleError, createMain),
+  }
+}

dff6b7c41274610b2f060f549b8bbaf2bf2ca7e0
Tamara Jordan <tamara.jordan1+coding@hotmail.com>
2020-01-02 14:21:23 +0000

    Stub a couple of tests

---
+++ src/commands/serve/command.spec.ts
@@ -0,0 +1,9 @@
+describe('Serve command', () => {
+  it.todo('exits with code 1 if a config path was not specified')
+
+  describe('when port is not a number', () => {
+    it.todo('Shows help if the given port is not a number')
+    it.todo('Logs an error if the given port is not a number')
+    it.todo('Exists with code 1 if the given port is not a number')
+  })
+})

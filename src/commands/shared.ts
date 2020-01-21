61a8be9ffe55f14534c6474eff1d4f1fbea9df72
Tamara Jordan <tamara.jordan1+coding@hotmail.com>
2020-01-21 10:23:25 +0000

    Nice spacing for problem logging

--- src/commands/shared.ts
+++ src/commands/shared.ts
@@ -6,7 +6,7 @@ import { inspect } from 'util'
 export type HandleError = (error: Error) => never
 export type CreateTypeValidator = (tsconfigPath: string, schemaPath?: string) => TypeValidator
 
-const groupBy = <T>(items: ReadonlyArray<T>, getKey: (item: T) => string): Map<string, ReadonlyArray<T>> =>
+const groupBy = <T>(items: ReadonlyArray<T>, getKey: (item: T) => string): Map<string, Readonly<T>[]> =>
   [...items]
     .sort((a, b) => getKey(a).localeCompare(getKey(b)))
     .reduce((map, item) => {
@@ -26,6 +26,7 @@ export const gatherValidationErrors = (problems: ReadonlyArray<Problem>): string
   Array.from(groupBy(problems, x => x.problemType))
     .map(([type, groupedByType]) => {
       return groupedByType
+        .sort((a, b) => a.path.localeCompare(b.path))
         .reduce<string[]>((accum, problem, i) => {
           const { path, message, value, showValue, schema, showSchema, definedBy, allowedValues } = problem
           const output = [`${blue(type)} ${blue(path)} ${message}`]
@@ -36,26 +37,17 @@ export const gatherValidationErrors = (problems: ReadonlyArray<Problem>): string
           if (definedBy) output.push(`${yellow('Defined by')}: ${definedBy}`)
           if (schema && showSchema) output.push(`${yellow('Schema:')} ${colorInspect(schema, 2)}`)
 
-          const prev = accum[i - 1]
+          const joinedOutput = output.join('\n')
 
-          accum.push(`${output.length > 1 || prev?.startsWith('\n') ? '\n' : ''}${output.join('\n')}`)
+          if (output.length > 1) {
+            const isPreviousMultiline = accum[i - 1]?.charCodeAt(0) === 10
+            accum.push(`${i === 0 || isPreviousMultiline ? '' : '\n'}${joinedOutput}\n`)
+          } else {
+            accum.push(joinedOutput)
+          }
+
           return accum
         }, [])
         .join(`\n`)
-
-      // const result = groupedByPath.map(({ message }) => {
-      //   const messagePrefix = blue(`${type} ${dataPath}`)
-      //   return `${messagePrefix} ${message}`
-      // })
-
-      // const { value, showValue, schema, showSchema, definedBy, allowedValues } = groupedByPath[0]
-
-      // if (value && showValue)
-      //   result.push(`${yellow('Value:')} ${colorInspect(value, dataPath === Problem.rootPath ? 0 : 4)}`)
-      // if (allowedValues) result.push(`${yellow('Allowed values')}: ${allowedValues}`)
-      // if (definedBy) result.push(`${yellow('Defined by')}: ${definedBy}`)
-      // if (schema && showSchema) result.push(`${yellow('Schema:')} ${colorInspect(schema, 2)}`)
-
-      // return `${result.join('\n')}\n`
     })
     .join('\n')

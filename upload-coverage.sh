c3de71052333d3562c095af6bb94d1cdc3ffd479
Tamara Jordan <tamara.jordan1+coding@hotmail.com>
2020-07-01 20:22:47 +0100

    ci(github actions): explicitly upload codecov using SHA and PR id

--- upload-coverage.sh
+++ upload-coverage.sh
@@ -12,5 +12,17 @@ function curl-harder() {
     return 1
 }
 
+# from https://github.com/codecov/codecov-action/issues/69
+EVENT_PAYLOAD_FILE=$1
+
+PRID=`jq ".number // .check_run.pull_requests[0].number" $EVENT_PAYLOAD_FILE`
+SHA=`jq -r ".pull_request.head.sha // .check_run.head_sha // .after" $EVENT_PAYLOAD_FILE`
+
 curl-harder -o codecov.sh https://codecov.io/bash
-bash codecov.sh -Z
+
+if [ $PRID = "null" ]
+then
+  bash codecov.sh -Z -C $SHA
+else
+  bash codecov.sh -Z -C $SHA -P $PRID
+fi

dc9a056c42d916ed43ce244050c1a6ab4b8d5dc2
Tamara Jordan <tamara.jordan1+coding@hotmail.com>
2020-07-06 20:00:41 +0100

    ci(github actions): fix semantic release in workflow

--- upload-coverage.sh
+++ upload-coverage.sh
@@ -15,6 +15,8 @@ function curl-harder() {
 # from https://github.com/codecov/codecov-action/issues/69
 EVENT_PAYLOAD_FILE=$1
 
+jq --raw-output . "$EVENT_PAYLOAD_FILE"
+
 PRID=`jq ".number // .check_run.pull_requests[0].number" $EVENT_PAYLOAD_FILE`
 SHA=`jq -r ".pull_request.head.sha // .check_run.head_sha // .after" $EVENT_PAYLOAD_FILE`
 

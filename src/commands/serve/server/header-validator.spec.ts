7bd0a10b25309427bc28e91e8c90ed633bda837d
Tamara Jordan <tamara.jordan1+coding@hotmail.com>
2020-07-14 19:13:27 +0100

    test: improve header-validator coverage

--- src/commands/serve/server/header-validator.spec.ts
+++ src/commands/serve/server/header-validator.spec.ts
@@ -66,4 +66,56 @@ describe('areHeadersValid', () => {
 
     expect(result).toMatchObject<HeaderValidationResult>({ success: false })
   })
+
+  describe('when an expected header is a comma separated value', () => {
+    it('returns false when only a single header value is sent', () => {
+      const expected = {
+        goodbye: 'cruel,world',
+      }
+
+      const result = areHeadersValid(expected, {
+        goodbye: 'cruel',
+      })
+
+      expect(result).toMatchObject<HeaderValidationResult>({ success: false })
+    })
+
+    it('returns false when they do not match', () => {
+      const expected = {
+        goodbye: 'cruel,world',
+      }
+
+      const result = areHeadersValid(expected, {
+        goodbye: ['cruel', 'orld'],
+      })
+
+      expect(result).toMatchObject<HeaderValidationResult>({ success: false })
+    })
+  })
+
+  describe('when expected is a single value', () => {
+    it('returns true when actual is an array that contains the correct value', () => {
+      const expected = {
+        hello: 'world',
+      }
+
+      const result = areHeadersValid(expected, {
+        hello: ['sweet', 'world'],
+      })
+
+      expect(result).toMatchObject<HeaderValidationResult>({ success: true })
+    })
+
+    it('returns true when actual is an array that does not contain the correct value', () => {
+      const expected = {
+        hello: 'world',
+      }
+
+      const result = areHeadersValid(expected, {
+        hello: ['sweet', 'gold'],
+      })
+
+      expect(result).toMatchObject<HeaderValidationResult>({ success: false })
+    })
+  })
 })

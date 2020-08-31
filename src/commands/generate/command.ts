7300eb3a586bde7c6db75e6f12894980c4074fbf
Tamara Jordan <tamara.jordan1+coding@hotmail.com>
2020-08-31 12:04:15 +0100

    refactor: add explicit return type for commands

--- src/commands/generate/command.ts
+++ src/commands/generate/command.ts
@@ -1,5 +1,4 @@
-import { GetRootDeps } from '../shared'
-import { Argv, CommandModule } from 'yargs'
+import { GetRootDeps, CommandModule } from '../shared'
 import * as opts from '~commands/options'
 import createHandler, { GenerateArgs } from './handler'
 import { getConfigTypes } from './config'
@@ -7,25 +6,23 @@ import { SchemaGenerator } from '~schema'
 import { generate } from './generate'
 import TsHelpers from '~schema/ts-helpers'
 
-const builder = (yargs: Argv): Argv<GenerateArgs> =>
-  yargs
-    .positional(opts.CONFIG_PATHS, opts.CONFIG_PATHS_OPTS)
-    .option(opts.TSCONFIG_PATH, opts.TSCONFIG_PATH_OPTS)
-    .option('outputPath', {
-      alias: ['o', 'output'],
-      type: 'string',
-      description: 'sets an output folder for the json schemas',
-      default: './json-schema',
-    })
-    .option(opts.FORCE_GENERATION, opts.FORCE_GENERATION_OPTS)
-    .option(opts.VERBOSE, opts.VERBOSE_OPTS)
-    .example(opts.EXAMPLE_GENERATE_COMMAND, opts.EXAMPLE_GENERATE_DESCRIPTION)
-
-export default function createGenerateCommand(getCommonDeps: GetRootDeps): CommandModule<{}, GenerateArgs> {
+export default function createGenerateCommand(getCommonDeps: GetRootDeps): CommandModule<GenerateArgs> {
   return {
     command: `generate <${opts.CONFIG_PATHS}..>`,
     describe: 'Generates a json schema for each type specified in the config file',
-    builder,
+    builder: (yargs) =>
+      yargs
+        .positional(opts.CONFIG_PATHS, opts.CONFIG_PATHS_OPTS)
+        .option(opts.TSCONFIG_PATH, opts.TSCONFIG_PATH_OPTS)
+        .option('outputPath', {
+          alias: ['o', 'output'],
+          type: 'string',
+          description: 'sets an output folder for the json schemas',
+          default: './json-schema',
+        })
+        .option(opts.FORCE_GENERATION, opts.FORCE_GENERATION_OPTS)
+        .option(opts.VERBOSE, opts.VERBOSE_OPTS)
+        .example(opts.EXAMPLE_GENERATE_COMMAND, opts.EXAMPLE_GENERATE_DESCRIPTION),
     handler: createHandler(({ verbose }) => {
       const { handleError, logger, reportMetric: reportMetric } = getCommonDeps(verbose)
       return {
--- src/commands/test/command.ts
+++ src/commands/test/command.ts
@@ -1,5 +1,4 @@
-import { GetRootDeps } from '../shared'
-import { Argv, CommandModule } from 'yargs'
+import { GetRootDeps, CommandModule } from '../shared'
 import * as opts from '~commands/options'
 import { createHandler, TestArgs, GetTestDeps } from './handler'
 import { runTests } from './test'
@@ -7,28 +6,7 @@ import loadConfig from '~config/load'
 import TypeValidatorFactory from '~validation'
 import { createHttpClient } from './http-client'
 
-const builder = (yargs: Argv): Argv<TestArgs> =>
-  yargs
-    .positional(opts.CONFIG_PATH, opts.CONFIG_PATH_OPTS)
-    .positional('baseURL', {
-      describe: 'the URL that your endpoints should be accessed through',
-      type: 'string',
-    })
-    .option('timeout', {
-      description: 'req/res timeout in ms',
-      type: 'number',
-    })
-    .option('rateLimit', {
-      description: 'minimum time in ms to wait between sending each request',
-      type: 'number',
-    })
-    .option(opts.SCHEMA_PATH, opts.SCHEMA_PATH_OPTS)
-    .option(opts.TSCONFIG_PATH, opts.TSCONFIG_PATH_OPTS)
-    .option(opts.FORCE_GENERATION, opts.FORCE_GENERATION_OPTS)
-    .option(opts.VERBOSE, opts.VERBOSE_OPTS)
-    .example(opts.EXAMPLE_TEST_COMMAND, opts.EXAMPLE_TEST_DESCRIPTION)
-
-export default function createTestCommand(getCommonDeps: GetRootDeps): CommandModule<{}, TestArgs> {
+export default function createTestCommand(getCommonDeps: GetRootDeps): CommandModule<TestArgs> {
   const getTestDeps: GetTestDeps = (args) => {
     const { force, tsconfigPath, schemaPath, verbose } = args
     const { handleError, logger, reportMetric: reportMetric } = getCommonDeps(verbose)
@@ -54,7 +32,26 @@ export default function createTestCommand(getCommonDeps: GetRootDeps): CommandMo
   return {
     command: 'test <configPath> <baseURL>',
     describe: 'Tests configured endpoints',
-    builder,
+    builder: (yargs) =>
+      yargs
+        .positional(opts.CONFIG_PATH, opts.CONFIG_PATH_OPTS)
+        .positional('baseURL', {
+          describe: 'the URL that your endpoints should be accessed through',
+          type: 'string',
+        })
+        .option('timeout', {
+          description: 'req/res timeout in ms',
+          type: 'number',
+        })
+        .option('rateLimit', {
+          description: 'minimum time in ms to wait between sending each request',
+          type: 'number',
+        })
+        .option(opts.SCHEMA_PATH, opts.SCHEMA_PATH_OPTS)
+        .option(opts.TSCONFIG_PATH, opts.TSCONFIG_PATH_OPTS)
+        .option(opts.FORCE_GENERATION, opts.FORCE_GENERATION_OPTS)
+        .option(opts.VERBOSE, opts.VERBOSE_OPTS)
+        .example(opts.EXAMPLE_TEST_COMMAND, opts.EXAMPLE_TEST_DESCRIPTION),
     handler: createHandler(getTestDeps),
   }
 }

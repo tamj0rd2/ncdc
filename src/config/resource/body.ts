33303d7e134245d02652d36a38a8efec5d481608
Tamara Jordan <tamara.jordan1+coding@hotmail.com>
2020-08-31 14:00:30 +0100

    refactor: encapsulate body match logic

--- src/config/resource/body.ts
+++ src/config/resource/body.ts
@@ -1,24 +1,40 @@
-import { isDeeplyEqual } from '~util'
+const isObject = (x: unknown): x is object => typeof x === 'object' && !!x
 
 export class Body {
-  private readonly isObject = typeof this.data === 'object'
+  private readonly isObject = isObject(this.data)
 
   constructor(private readonly data: Data) {}
 
-  public serialize(): string {
+  public serialize = (): string => {
     return this.isObject ? JSON.stringify(this.data) : this.data?.toString()
   }
 
-  public matches(bodyToCompare: unknown): boolean {
-    return isDeeplyEqual(this.data, bodyToCompare)
+  public matches = (bodyToCompare: unknown): boolean => {
+    return this.isDeeplyEqual(this.data, bodyToCompare)
   }
 
-  public get(): Data {
+  public get = (): Data => {
     return this.data
   }
 
-  public toString(): string {
+  public toString = (): string => {
     const shortenedBody = this.data?.toString().substr(0, 30)
     return `${shortenedBody}${shortenedBody && shortenedBody.length >= 30 ? '...' : ''}`
+  }
+
+  private isDeeplyEqual = (expected: unknown, actual: unknown): boolean => {
+    if (isObject(expected)) {
+      if (!isObject(actual)) return false
+
+      for (const key in expected) {
+        const expectedValue = expected[key as keyof typeof expected]
+        const actualValue = actual[key as keyof typeof actual]
+        if (!this.isDeeplyEqual(expectedValue, actualValue)) return false
+      }
+
+      return true
+    }
+
+    return expected === actual
   }
 }

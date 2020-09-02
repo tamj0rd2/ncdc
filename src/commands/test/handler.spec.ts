0c7c25046dc73c63feec13321446ad7df5c1b2e0
Tamara Jordan <tamara.jordan1+coding@hotmail.com>
2020-09-02 19:33:02 +0100

    test(test): do not use shared mocks

--- src/commands/test/handler.spec.ts
+++ src/commands/test/handler.spec.ts
@@ -1,9 +1,6 @@
-import { mockFn, randomString, mocked, mockObj, randomNumber } from '~test-helpers'
+import { mockFn, randomString, mockObj, randomNumber } from '~test-helpers'
 import { HandleError } from '~commands/shared'
 import { createHandler, TestArgs, GetTestDeps, GetTypeValidator, RunTests } from './handler'
-import { resolve } from 'path'
-import { existsSync } from 'fs'
-import { TypeValidator } from '~validation'
 import { NcdcLogger } from '~logger'
 import { LoadConfig } from '~config/load'
 import { ValidatedTestConfig, transformConfigs } from './config'
@@ -16,127 +13,245 @@ import {
 } from '~config/errors'
 
 jest.disableAutomock()
-jest.mock('fs')
-jest.mock('path')
 
-const mockedHandleError = mockFn<HandleError>()
-const mockedTypeValidator = mockObj<TypeValidator>({})
-const mockGetTypeValidator = mockFn<GetTypeValidator>()
-const mockedExistsSync = mocked(existsSync)
-const mockedResolve = mocked(resolve)
-const resolvedTsconfigPath = randomString('resolved-tsconfig')
-const mockedLogger = mockObj<NcdcLogger>({ warn: jest.fn() })
-const mockedRunTests = mockFn<RunTests>()
-const mockedLoadConfig = mockFn<LoadConfig<ValidatedTestConfig>>()
-const getTestDeps = mockFn<GetTestDeps>()
+describe('test handler', () => {
+  function createTestDeps() {
+    const mockHandleError = mockFn<HandleError>()
+    const mockGetTypeValidator = mockFn<GetTypeValidator>()
+    const mockLogger = mockObj<NcdcLogger>({ warn: jest.fn() })
+    const mockRunTests = mockFn<RunTests>()
+    const mockLoadConfig = mockFn<LoadConfig<ValidatedTestConfig>>()
+    const mockGetTestDeps = mockFn<GetTestDeps>()
 
-beforeEach(() => {
-  jest.resetAllMocks()
-  mockedExistsSync.mockReturnValue(true)
-  mockedResolve.mockReturnValue(resolvedTsconfigPath)
-  mockGetTypeValidator.mockResolvedValue(mockedTypeValidator)
-  getTestDeps.mockReturnValue({
-    getTypeValidator: mockGetTypeValidator,
-    handleError: mockedHandleError,
-    loadConfig: mockedLoadConfig,
-    logger: mockedLogger,
-    runTests: mockedRunTests,
-  })
-})
-
-const handler = createHandler(getTestDeps)
-
-describe('cli arg validation', () => {
-  it('returns an error if a configPath is not given', async () => {
-    const args: TestArgs = {
-      force: false,
-      tsconfigPath: randomString(),
-      verbose: false,
-      timeout: randomNumber(),
+    return {
+      mockHandleError,
+      mockGetTypeValidator,
+      mockLogger,
+      mockRunTests,
+      mockLoadConfig,
+      mockGetTestDeps,
+      handler: createHandler(mockGetTestDeps),
     }
+  }
+
+  beforeEach(() => {
+    jest.resetAllMocks()
+    // mockExistsSync.mockReturnValue(true)
+    // mockResolve.mockReturnValue(dummyResolvedTsconfigPath)
+    // mockGetTypeValidator.mockResolvedValue(mockTypeValidator)
+    // mockGetTestDeps.mockReturnValue({
+    //   getTypeValidator: mockGetTypeValidator,
+    //   handleError: mockHandleError,
+    //   loadConfig: mockLoadConfig,
+    //   logger: mockLogger,
+    //   runTests: mockRunTests,
+    // })
+  })
+
+  afterEach(() => jest.resetAllMocks())
+
+  describe('cli arg validation', () => {
+    it('returns an error if a configPath is not given', async () => {
+      const {
+        handler,
+        mockGetTestDeps,
+        mockGetTypeValidator,
+        mockHandleError,
+        mockLoadConfig,
+        mockLogger,
+        mockRunTests,
+      } = createTestDeps()
+      mockGetTestDeps.mockReturnValue({
+        getTypeValidator: mockGetTypeValidator,
+        handleError: mockHandleError,
+        loadConfig: mockLoadConfig,
+        logger: mockLogger,
+        runTests: mockRunTests,
+      })
+
+      await handler({
+        force: false,
+        tsconfigPath: randomString(),
+        verbose: false,
+        timeout: randomNumber(),
+      })
+
+      expect(mockHandleError).toBeCalledWith({ message: 'configPath must be specified' })
+    })
+
+    it('returns an error if a baseURL is not given', async () => {
+      const {
+        handler,
+        mockGetTestDeps,
+        mockGetTypeValidator,
+        mockHandleError,
+        mockLoadConfig,
+        mockLogger,
+        mockRunTests,
+      } = createTestDeps()
+      mockGetTestDeps.mockReturnValue({
+        getTypeValidator: mockGetTypeValidator,
+        handleError: mockHandleError,
+        loadConfig: mockLoadConfig,
+        logger: mockLogger,
+        runTests: mockRunTests,
+      })
+
+      await handler({
+        force: false,
+        tsconfigPath: randomString(),
+        configPath: randomString(),
+        verbose: false,
+        timeout: randomNumber(),
+      })
+
+      expect(mockHandleError).toBeCalledWith({ message: 'baseURL must be specified' })
+    })
+  })
+
+  const args: TestArgs = {
+    force: false,
+    tsconfigPath: randomString('tsconfig-path'),
+    configPath: randomString('config-path'),
+    baseURL: randomString('baseURL'),
+    schemaPath: randomString('schema-path'),
+    verbose: false,
+    timeout: randomNumber(),
+  }
+
+  it('calls loadConfig with the correct args', async () => {
+    const {
+      handler,
+      mockGetTestDeps,
+      mockGetTypeValidator,
+      mockHandleError,
+      mockLoadConfig,
+      mockLogger,
+      mockRunTests,
+    } = createTestDeps()
+    mockGetTestDeps.mockReturnValue({
+      getTypeValidator: mockGetTypeValidator,
+      handleError: mockHandleError,
+      loadConfig: mockLoadConfig,
+      logger: mockLogger,
+      runTests: mockRunTests,
+    })
+    mockLoadConfig.mockResolvedValue({
+      absoluteFixturePaths: [],
+      configs: [],
+    })
 
     await handler(args)
 
-    expect(mockedHandleError).toBeCalledWith({ message: 'configPath must be specified' })
+    expect(mockLoadConfig).toBeCalledWith(args.configPath, expect.any(Function), transformConfigs, true)
   })
 
-  it('returns an error if a baseURL is not given', async () => {
-    const args: TestArgs = {
-      force: false,
-      tsconfigPath: randomString(),
-      configPath: randomString(),
-      verbose: false,
-      timeout: randomNumber(),
-    }
+  const badStatuses = [
+    new InvalidBodyTypeError('file path', 'message'),
+    new ServiceConfigInvalidError('file path', ['error1']),
+    new ServiceConfigReadError('file path', 'message'),
+  ] as const
+  it.each(badStatuses.map((x) => [x]))(`handles a %o response from loadConfig`, async (error) => {
+    const {
+      handler,
+      mockGetTestDeps,
+      mockGetTypeValidator,
+      mockHandleError,
+      mockLoadConfig,
+      mockLogger,
+      mockRunTests,
+    } = createTestDeps()
+    mockGetTestDeps.mockReturnValue({
+      getTypeValidator: mockGetTypeValidator,
+      handleError: mockHandleError,
+      loadConfig: mockLoadConfig,
+      logger: mockLogger,
+      runTests: mockRunTests,
+    })
+    mockLoadConfig.mockRejectedValue(error)
 
     await handler(args)
 
-    expect(mockedHandleError).toBeCalledWith({ message: 'baseURL must be specified' })
-  })
-})
-
-const args: TestArgs = {
-  force: false,
-  tsconfigPath: randomString('tsconfig-path'),
-  configPath: randomString('config-path'),
-  baseURL: randomString('baseURL'),
-  schemaPath: randomString('schema-path'),
-  verbose: false,
-  timeout: randomNumber(),
-}
-
-it('calls loadConfig with the correct args', async () => {
-  mockedLoadConfig.mockResolvedValue({
-    absoluteFixturePaths: [],
-    configs: [],
+    expect(mockHandleError).toBeCalledWith(expect.objectContaining({ message: error.message }))
   })
 
-  await handler(args)
+  it('handles there being no configs to run as an error', async () => {
+    const {
+      handler,
+      mockGetTestDeps,
+      mockGetTypeValidator,
+      mockHandleError,
+      mockLoadConfig,
+      mockLogger,
+      mockRunTests,
+    } = createTestDeps()
+    mockGetTestDeps.mockReturnValue({
+      getTypeValidator: mockGetTypeValidator,
+      handleError: mockHandleError,
+      loadConfig: mockLoadConfig,
+      logger: mockLogger,
+      runTests: mockRunTests,
+    })
+    mockLoadConfig.mockRejectedValue(new NoServiceResourcesError('file path'))
 
-  expect(mockedLoadConfig).toBeCalledWith(args.configPath, expect.any(Function), transformConfigs, true)
-})
+    await handler(args)
 
-const badStatuses = [
-  new InvalidBodyTypeError('file path', 'message'),
-  new ServiceConfigInvalidError('file path', ['error1']),
-  new ServiceConfigReadError('file path', 'message'),
-] as const
-it.each(badStatuses.map((x) => [x]))(`handles a %o response from loadConfig`, async (error) => {
-  mockedLoadConfig.mockRejectedValue(error)
-
-  await handler(args)
-
-  expect(mockedHandleError).toBeCalledWith(expect.objectContaining({ message: error.message }))
-})
-
-it('handles there being no configs to run as an error', async () => {
-  mockedLoadConfig.mockRejectedValue(new NoServiceResourcesError('file path'))
-
-  await handler(args)
-
-  expect(mockedHandleError).toBeCalledWith(
-    expect.objectContaining({ message: expect.stringContaining('No configs to test') }),
-  )
-})
-
-it('calls runTests with the correct arguments', async () => {
-  const configs = [new ResourceBuilder().build()]
-  mockedLoadConfig.mockResolvedValue({ configs, absoluteFixturePaths: [] })
-
-  await handler(args)
-
-  expect(mockedHandleError).not.toBeCalled()
-  expect(mockedRunTests).toBeCalledWith(args.baseURL, configs, expect.any(Function))
-})
-
-it('handles errors thrown by testConfigs', async () => {
-  mockedLoadConfig.mockResolvedValue({
-    absoluteFixturePaths: [],
-    configs: [new ResourceBuilder().build()],
+    expect(mockHandleError).toBeCalledWith(
+      expect.objectContaining({ message: expect.stringContaining('No configs to test') }),
+    )
   })
-  mockedRunTests.mockResolvedValue('Failure')
 
-  await handler(args)
+  it('calls runTests with the correct arguments', async () => {
+    const {
+      handler,
+      mockGetTestDeps,
+      mockGetTypeValidator,
+      mockHandleError,
+      mockLoadConfig,
+      mockLogger,
+      mockRunTests,
+    } = createTestDeps()
+    mockGetTestDeps.mockReturnValue({
+      getTypeValidator: mockGetTypeValidator,
+      handleError: mockHandleError,
+      loadConfig: mockLoadConfig,
+      logger: mockLogger,
+      runTests: mockRunTests,
+    })
+    const configs = [new ResourceBuilder().build()]
+    mockLoadConfig.mockResolvedValue({ configs, absoluteFixturePaths: [] })
 
-  expect(mockedHandleError).toBeCalledWith(expect.objectContaining({ message: 'Not all tests passed' }))
+    await handler(args)
+
+    expect(mockHandleError).not.toBeCalled()
+    expect(mockRunTests).toBeCalledWith(args.baseURL, configs, expect.any(Function))
+  })
+
+  it('handles errors thrown by testConfigs', async () => {
+    const {
+      handler,
+      mockGetTestDeps,
+      mockGetTypeValidator,
+      mockHandleError,
+      mockLoadConfig,
+      mockLogger,
+      mockRunTests,
+    } = createTestDeps()
+    mockGetTestDeps.mockReturnValue({
+      getTypeValidator: mockGetTypeValidator,
+      handleError: mockHandleError,
+      loadConfig: mockLoadConfig,
+      logger: mockLogger,
+      runTests: mockRunTests,
+    })
+    mockLoadConfig.mockResolvedValue({
+      absoluteFixturePaths: [],
+      configs: [new ResourceBuilder().build()],
+    })
+    mockRunTests.mockResolvedValue('Failure')
+
+    await handler(args)
+
+    expect(mockHandleError).toBeCalledWith(expect.objectContaining({ message: 'Not all tests passed' }))
+  })
 })
--- src/commands/test/test.spec.ts
+++ src/commands/test/test.spec.ts
@@ -1,60 +1,103 @@
 import { runTests, GetTypeValidator } from './test'
 import { randomString, mockFn, mockObj, randomNumber } from '~test-helpers'
 import stripAnsi from 'strip-ansi'
-import { Resource } from '~config'
 import { FetchResource } from './http-client'
 import { TypeValidator } from '~validation'
 import { ResourceBuilder } from '~config'
 import { Logger } from 'winston'
 import { ReportMetric } from '~commands/shared'
-import { OperationResult } from '~metrics'
+import { NcdcLogger } from '~logger'
 
 jest.disableAutomock()
 
 describe('test configs', () => {
-  const baseUrl = randomString('http://example') + '.com'
-  const mockLogger = mockObj<Logger>({ error: jest.fn(), info: jest.fn() })
-  const mockFetchResource = mockFn<FetchResource>()
-  const mockTypeValidator = mockObj<TypeValidator>({ validate: jest.fn() })
-  const mockGetTypeValidator = mockFn<GetTypeValidator>()
-  const mockReportMetric = mockFn<ReportMetric>()
+  function createTestDeps() {
+    const baseUrl = randomString('http://example') + '.com'
+    const mockLogger = mockObj<Logger>({ error: jest.fn(), info: jest.fn() })
+    const mockFetchResource = mockFn<FetchResource>()
+    const mockTypeValidator = mockObj<TypeValidator>({ validate: jest.fn() })
+    const mockGetTypeValidator = mockFn<GetTypeValidator>()
+    const dummyReportMetric: ReportMetric = () => ({
+      fail: jest.fn(),
+      subMetric: jest.fn(),
+      success: jest.fn(),
+    })
+
+    return {
+      baseUrl,
+      mockLogger,
+      mockFetchResource,
+      mockTypeValidator,
+      mockGetTypeValidator,
+      dummyReportMetric,
+    }
+  }
 
   beforeEach(() => {
-    jest.resetAllMocks()
-    mockGetTypeValidator.mockResolvedValue(mockTypeValidator)
-    mockReportMetric.mockReturnValue(
-      mockObj<OperationResult>({ fail: jest.fn(), success: jest.fn() }),
-    )
+    // jest.resetAllMocks()
+    // mockGetTypeValidator.mockResolvedValue(mockTypeValidator)
+    // dummyReportMetric.mockReturnValue(
+    //   mockObj<OperationResult>({ fail: jest.fn(), success: jest.fn() }),
+    // )
   })
 
-  // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
-  const act = (...configs: Resource[]) =>
-    runTests(baseUrl, mockFetchResource, configs, mockGetTypeValidator, mockLogger, mockReportMetric)
+  afterEach(() => jest.resetAllMocks())
 
-  const getLoggedMessage = (method: 'error' | 'info', callIndex = 0): string =>
-    stripAnsi((mockLogger[method].mock.calls[callIndex][0] as unknown) as string)
+  // const act = (...configs: Resource[]) =>
+  //   runTests(baseUrl, mockFetchResource, configs, mockGetTypeValidator, mockLogger, dummyReportMetric)
+
+  const getLoggedMessage = (method: 'error' | 'info', logger: jest.Mocked<NcdcLogger>): string =>
+    stripAnsi((logger[method].mock.calls[0][0] as unknown) as string)
 
   it('calls fetchResource with the correct args', async () => {
+    const {
+      baseUrl,
+      mockFetchResource,
+      mockGetTypeValidator,
+      mockLogger,
+      dummyReportMetric,
+    } = createTestDeps()
     const config = new ResourceBuilder().build()
     mockFetchResource.mockResolvedValue({ status: randomNumber() })
 
-    await act(config)
+    await runTests(baseUrl, mockFetchResource, [config], mockGetTypeValidator, mockLogger, dummyReportMetric)
 
     expect(mockFetchResource).toBeCalledWith(config)
   })
 
   it('logs a failure when fetching a resource throws', async () => {
+    const {
+      baseUrl,
+      mockFetchResource,
+      mockGetTypeValidator,
+      mockLogger,
+      dummyReportMetric,
+    } = createTestDeps()
     const config = new ResourceBuilder().withName('Bob').withEndpoint('/jim').build()
     const errorMessage = randomString('error message')
     mockFetchResource.mockRejectedValue(new Error(errorMessage))
 
-    const result = await act(config)
+    const result = await runTests(
+      baseUrl,
+      mockFetchResource,
+      [config],
+      mockGetTypeValidator,
+      mockLogger,
+      dummyReportMetric,
+    )
 
-    expect(getLoggedMessage('error')).toEqual(`FAILED: Bob - ${baseUrl}/jim\n${errorMessage}`)
+    expect(getLoggedMessage('error', mockLogger)).toEqual(`FAILED: Bob - ${baseUrl}/jim\n${errorMessage}`)
     expect(result).toEqual('Failure')
   })
 
   it('returns a failed message when the status code does not match', async () => {
+    const {
+      baseUrl,
+      mockFetchResource,
+      mockGetTypeValidator,
+      mockLogger,
+      dummyReportMetric,
+    } = createTestDeps()
     const expectedStatus = randomNumber()
     const config = new ResourceBuilder()
       .withName('Bob')
@@ -63,9 +106,16 @@ describe('test configs', () => {
       .build()
     mockFetchResource.mockResolvedValue({ status: 123 })
 
-    const result = await act(config)
+    const result = await runTests(
+      baseUrl,
+      mockFetchResource,
+      [config],
+      mockGetTypeValidator,
+      mockLogger,
+      dummyReportMetric,
+    )
 
-    expect(getLoggedMessage('error')).toEqual(
+    expect(getLoggedMessage('error', mockLogger)).toEqual(
       `FAILED: Bob - ${baseUrl}/jim\nExpected status code ${expectedStatus} but got 123`,
     )
     expect(result).toEqual('Failure')
@@ -73,6 +123,13 @@ describe('test configs', () => {
 
   describe('validation when a response body is configured', () => {
     it('does not return a failure message when the body matches the specified object', async () => {
+      const {
+        baseUrl,
+        mockFetchResource,
+        mockGetTypeValidator,
+        mockLogger,
+        dummyReportMetric,
+      } = createTestDeps()
       const config = new ResourceBuilder()
         .withName('Bob')
         .withEndpoint('/jim')
@@ -83,13 +140,27 @@ describe('test configs', () => {
         data: { hello: ['to', 'the', { world: 'earth' }], cya: 'later', mate: 23 },
       })
 
-      const result = await act(config)
+      const result = await runTests(
+        baseUrl,
+        mockFetchResource,
+        [config],
+        mockGetTypeValidator,
+        mockLogger,
+        dummyReportMetric,
+      )
 
-      expect(getLoggedMessage('info')).toEqual(`PASSED: Bob - ${baseUrl}/jim`)
+      expect(getLoggedMessage('info', mockLogger)).toEqual(`PASSED: Bob - ${baseUrl}/jim`)
       expect(result).toEqual('Success')
     })
 
     it('returns a failure message when the response body is undefined', async () => {
+      const {
+        baseUrl,
+        mockFetchResource,
+        mockGetTypeValidator,
+        mockLogger,
+        dummyReportMetric,
+      } = createTestDeps()
       const config = new ResourceBuilder()
         .withName('Bob')
         .withEndpoint('/jim')
@@ -97,13 +168,29 @@ describe('test configs', () => {
         .build()
       mockFetchResource.mockResolvedValue({ status: 200 })
 
-      const result = await act(config)
+      const result = await runTests(
+        baseUrl,
+        mockFetchResource,
+        [config],
+        mockGetTypeValidator,
+        mockLogger,
+        dummyReportMetric,
+      )
 
-      expect(getLoggedMessage('error')).toEqual(`FAILED: Bob - ${baseUrl}/jim\nNo response body was received`)
+      expect(getLoggedMessage('error', mockLogger)).toEqual(
+        `FAILED: Bob - ${baseUrl}/jim\nNo response body was received`,
+      )
       expect(result).toEqual('Failure')
     })
 
     it('returns a failure message when the body does not match the specified object', async () => {
+      const {
+        baseUrl,
+        mockFetchResource,
+        mockGetTypeValidator,
+        mockLogger,
+        dummyReportMetric,
+      } = createTestDeps()
       const config = new ResourceBuilder()
         .withName('Bob')
         .withEndpoint('/jim')
@@ -112,9 +199,16 @@ describe('test configs', () => {
       const actualResponseBody = { hello: 'world' }
       mockFetchResource.mockResolvedValue({ status: 200, data: actualResponseBody })
 
-      const result = await act(config)
+      const result = await runTests(
+        baseUrl,
+        mockFetchResource,
+        [config],
+        mockGetTypeValidator,
+        mockLogger,
+        dummyReportMetric,
+      )
 
-      expect(getLoggedMessage('error')).toEqual(
+      expect(getLoggedMessage('error', mockLogger)).toEqual(
         `FAILED: Bob - ${baseUrl}/jim\nThe response body was not deeply equal to your configured fixture\nReceived:\n{ hello: 'world' }`,
       )
       expect(result).toEqual('Failure')
@@ -123,6 +217,14 @@ describe('test configs', () => {
 
   describe('when the config has a response type specified', () => {
     it('calls the type validator with the correct params', async () => {
+      const {
+        baseUrl,
+        mockFetchResource,
+        mockGetTypeValidator,
+        mockLogger,
+        dummyReportMetric,
+        mockTypeValidator,
+      } = createTestDeps()
       const config = new ResourceBuilder()
         .withName('Bob')
         .withEndpoint('/jim')
@@ -132,13 +234,29 @@ describe('test configs', () => {
       const actualData = randomString('data')
       mockFetchResource.mockResolvedValue({ status: 200, data: actualData })
       mockTypeValidator.validate.mockResolvedValue({ success: true })
+      mockGetTypeValidator.mockResolvedValue(mockTypeValidator)
 
-      await act(config)
+      await runTests(
+        baseUrl,
+        mockFetchResource,
+        [config],
+        mockGetTypeValidator,
+        mockLogger,
+        dummyReportMetric,
+      )
 
       expect(mockTypeValidator.validate).toBeCalledWith(actualData, config.response.type)
     })
 
-    it('does not return a failure message when the types match', async () => {
+    it('logs a success message when the types match', async () => {
+      const {
+        baseUrl,
+        mockFetchResource,
+        mockGetTypeValidator,
+        mockLogger,
+        dummyReportMetric,
+        mockTypeValidator,
+      } = createTestDeps()
       const config = new ResourceBuilder()
         .withName('Bob')
         .withEndpoint('/jim')
@@ -147,14 +265,30 @@ describe('test configs', () => {
         .build()
       mockTypeValidator.validate.mockResolvedValue({ success: true })
       mockFetchResource.mockResolvedValue({ status: 200 })
+      mockGetTypeValidator.mockResolvedValue(mockTypeValidator)
 
-      const result = await act(config)
+      const result = await runTests(
+        baseUrl,
+        mockFetchResource,
+        [config],
+        mockGetTypeValidator,
+        mockLogger,
+        dummyReportMetric,
+      )
 
-      expect(getLoggedMessage('info')).toEqual(`PASSED: Bob - ${baseUrl}/jim`)
+      expect(getLoggedMessage('info', mockLogger)).toEqual(`PASSED: Bob - ${baseUrl}/jim`)
       expect(result).toEqual('Success')
     })
 
     it('returns a failure message when the types do not match', async () => {
+      const {
+        baseUrl,
+        mockFetchResource,
+        mockGetTypeValidator,
+        mockLogger,
+        dummyReportMetric,
+        mockTypeValidator,
+      } = createTestDeps()
       const config = new ResourceBuilder()
         .withName('Bob')
         .withEndpoint('/jim')
@@ -165,13 +299,23 @@ describe('test configs', () => {
       const error2 = randomString('error2')
       mockTypeValidator.validate.mockResolvedValue({ success: false, errors: [error1, error2] })
       mockFetchResource.mockResolvedValue({ status: 200 })
+      mockGetTypeValidator.mockResolvedValue(mockTypeValidator)
 
-      const result = await act(config)
+      const result = await runTests(
+        baseUrl,
+        mockFetchResource,
+        [config],
+        mockGetTypeValidator,
+        mockLogger,
+        dummyReportMetric,
+      )
 
       const expectedPart1 = `FAILED: Bob - ${baseUrl}/jim\n`
       const expectedPart2 = `The received body does not match the type ${config.response.type}\n`
       const expectedPart3 = `${error1}\n${error2}`
-      expect(getLoggedMessage('error')).toEqual(`${expectedPart1}${expectedPart2}${expectedPart3}`)
+      expect(getLoggedMessage('error', mockLogger)).toEqual(
+        `${expectedPart1}${expectedPart2}${expectedPart3}`,
+      )
       expect(result).toEqual('Failure')
     })
   })

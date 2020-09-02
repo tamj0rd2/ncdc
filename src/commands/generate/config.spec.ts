bbb219aa03be79b75e030136e02b4d9f2b3eb51c
Tamara Jordan <tamara.jordan1+coding@hotmail.com>
2020-09-02 18:08:31 +0100

    test(generate): do not use shared mocks

--- src/commands/generate/config.spec.ts
+++ src/commands/generate/config.spec.ts
@@ -9,16 +9,19 @@ jest.mock('~io')
 jest.mock('~config/validate')
 
 describe('getConfigTypes', () => {
-  const mockReadYamlAsync = mockFn(readYamlAsync)
-  const mockValidateConfig = mockFn(validateRawConfig)
-
-  beforeEach(() => {
-    mockValidateConfig.mockReturnValue({ success: true, validatedConfigs: [] })
-  })
+  function createTestDeps() {
+    return {
+      getConfigTypes,
+      mockReadYamlAsync: mockFn(readYamlAsync),
+      mockValidateRawConfig: mockFn(validateRawConfig),
+    }
+  }
 
   afterEach(() => jest.resetAllMocks())
 
   it('calls readYamlAsync with the correct args for each config', async () => {
+    const { getConfigTypes, mockReadYamlAsync, mockValidateRawConfig } = createTestDeps()
+    mockValidateRawConfig.mockReturnValue({ success: true, validatedConfigs: [] })
     const configPaths = [randomString('path1'), randomString('path2')]
 
     await getConfigTypes(configPaths)
@@ -29,22 +32,25 @@ describe('getConfigTypes', () => {
 
   describe('config file validation', () => {
     it('validates each config with the correct args', async () => {
+      const { getConfigTypes, mockReadYamlAsync, mockValidateRawConfig } = createTestDeps()
       const configPaths = [randomString('path1'), randomString('path2')]
       const config1 = { name: 'hello' }
       const config2 = { name: 'world' }
       mockReadYamlAsync.mockResolvedValueOnce(config1).mockResolvedValueOnce(config2)
+      mockValidateRawConfig.mockReturnValue({ success: true, validatedConfigs: [] })
 
       await getConfigTypes(configPaths)
 
-      expect(mockValidateConfig).nthCalledWith(1, config1)
-      expect(mockValidateConfig).nthCalledWith(2, config2)
+      expect(mockValidateRawConfig).nthCalledWith(1, config1)
+      expect(mockValidateRawConfig).nthCalledWith(2, config2)
     })
 
     it("throws an error containing the details of each config's validation failures", async () => {
+      const { getConfigTypes, mockReadYamlAsync, mockValidateRawConfig } = createTestDeps()
       const configPaths = ['path1', randomString('path2'), 'path3']
       mockReadYamlAsync.mockResolvedValue({ hello: 'world' })
 
-      mockValidateConfig
+      mockValidateRawConfig
         .mockReturnValueOnce({ success: false, errors: ['whoopsie', 'daisy'] })
         .mockReturnValueOnce({ success: true, validatedConfigs: [] })
         .mockReturnValueOnce({ success: false, errors: ['oh no!'] })
@@ -58,10 +64,11 @@ describe('getConfigTypes', () => {
   })
 
   it('can return a list of types', async () => {
+    const { getConfigTypes, mockValidateRawConfig } = createTestDeps()
     const configPaths = [randomString('path1'), randomString('path2')]
     const createConfig = (reqType?: string, resType?: string): GenerateConfig =>
       mockObj<GenerateConfig>({ request: { type: reqType }, response: { type: resType } })
-    mockValidateConfig
+    mockValidateRawConfig
       .mockReturnValueOnce({
         success: true,
         validatedConfigs: [createConfig('RType'), createConfig(undefined, 'Delta')],

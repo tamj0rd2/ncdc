35c8b49f593b0dda5620f0e7ba89ad501f499411
Tamara Jordan <tamara.jordan1+coding@hotmail.com>
2021-01-20 23:27:23 +0000

    Make generate work

--- src/api/generate.ts
+++ src/api/generate.ts
@@ -1,20 +1,42 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
-/* eslint-disable @typescript-eslint/no-empty-interface */
+import { generate as coreGenerate } from '~commands/generate/generate'
 import { ReportMetric } from '~commands/shared'
+import { Type } from '~config/resource/type'
 import { NcdcLogger } from '~logger'
-import { GenerateService } from './types'
+import { SchemaGenerator, TsHelpers } from '~schema'
+import { CommonConfig, GenerateService } from './types'
 
-export function generate(
+export async function generate(
   services: GenerateService[],
   config: GenerateConfig,
   deps: GenerateDeps,
-): Promise<GenerateResults> {
-  throw new Error('Not yet implemented')
+): Promise<void> {
+  const types = new Set(
+    services
+      .flatMap((s) => s.resources.flatMap((r) => [r.request.type, r.response.type]))
+      .filter((x): x is Type => !!x),
+  )
+
+  if (!types.size) {
+    deps.logger.warn('No types were specified in the given config file')
+    return
+  }
+
+  const tsHelpers = new TsHelpers(deps.reportMetric, deps.logger)
+  const schemaGenerator = new SchemaGenerator(
+    tsHelpers.createProgram(config.tsconfigPath, { shouldTypecheck: !config.force ?? true }),
+  )
+  schemaGenerator.init()
+
+  await coreGenerate(schemaGenerator, Array.from(types), config.outputPath)
+  deps.logger.info('JSON schemas have been written to disk')
 }
 
-export interface GenerateResults {}
+export type GenerateResults = void
 
-export interface GenerateConfig {}
+export interface GenerateConfig extends CommonConfig {
+  force?: boolean
+  outputPath: string
+}
 
 interface GenerateDeps {
   logger: NcdcLogger
